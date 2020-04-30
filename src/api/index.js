8b147e84f6764dc35f90d4da5da72e048a9f364e
ChungHao Hsieh <chunghaohsieh@gmail.com>
2020-04-30 16:20:40 -0400

    day1

---
+++ src/api/index.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        return {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate
+        }
+    } catch (error) {
+
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+        return data.map(data => ({
+            confirmed: data.confirmed.total,
+            deaths: data.deaths.total,
+            date: data.reportDate
+        }));
+
+    } catch (error) {
+
+    }
+}
+
+
+export const fetchCountries = async () => {
+    try {
+        const { data: { countries } } = await axios.get(`${url}/countries`);
+
+        return countries.map(country => country.name);
+    } catch (error) {
+
+    }
+}
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from '@material-ui/core';
+
+import styles from './CountryPicker.module.css';
+
+import { fetchCountries } from '../../api';
+
+const CountryPicker = ({ handleCountryChange }) => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchCountry = async () => {
+            setFetchedCountries(await fetchCountries());
+        }
+        fetchCountry();
+    }, [setFetchedCountries])
+
+    console.log(fetchedCountries);
+
+    return (
+        <FormControl className={styles.formformControl}>
+            <NativeSelect defaultValue="" onChange={event => handleCountryChange(event.target.value)}>
+                {fetchedCountries.map((country, i) => (
+                    <option key={i} value={country}>{country}</option>
+                ))}
+            </NativeSelect>
+        </FormControl>
+    )
+}
+
+export default CountryPicker;
